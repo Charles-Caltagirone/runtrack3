fb81209a8164c255a7e7bc230907971ec762c73e
Charles-Caltagirone <charles.dewidehem-caltagirone@laplateforme.io>
2023-03-28 09:41:48 +0200

    fix bug, projet terminé

--- jour01/job07/script.js
+++ jour01/job07/script.js
@@ -1,43 +1,49 @@
 // console.log("Hello Javasript!");
+let date = new Date("2023-03-27");
 
-let currentDate = new Date("04 May 2020");
+const options = {weekday: 'long', year: 'numeric', month:'long', day:'numeric'};
+let dateStr = new Date(date).toLocaleDateString("fr-FR",options);
 
-let getDate = currentDate.getDate();
-let getDay = currentDate.getDay();
-let getMonth = currentDate.getMonth();
-let getYear = currentDate.getFullYear();
-
-let JourAn = new Date("04 May 2020");
-let Pacques = new Date("2020", "03", "13");
-let FeteTravail = new Date("2020", "04", "01");
-let Victoire1945 = new Date("2020", "04", "08");
-let Acsension = new Date("2020", "04", "21");
-let Pentecote = new Date("2020", "05", "01");
-let FeteNationale = new Date("2020","06", "14");
-let Assomption = new Date("2020", "07", "15");
-let Toussaint = new Date("2020", "10", "01");
-let Armistice = new Date("2020", "10", "11");
-let Noel = new Date("2020", "11", "25");
+// console.log(date.toLocaleDateString("fr"));
+// let date = date.toLocaleDateString("fr");
 
 
-function jourtravaille(date){
-    
-if(date == JourAn || date == FeteTravail){
-    console.log(`Le ${getDate}  ${getMonth}  ${getYear} est un jour férié`);
-}else if(getDay == 6 || getDay == 0){
-    console.log(`Non, ${getDate}  ${getMonth}  ${getYear} est un week-end`);
+// let getDate = date.getDate();
+let getDay = date.getDay();
+// let getMonth = date.getMonth();
+// let getYear= date.getFullYear();
+ 
+let jourAn = new Date("2020-01-01").toLocaleDateString("fr-FR",options);
+// let JourAnFR = JourAn.toLocaleDateString("fr");
+let pacques = new Date("2020-04-13").toLocaleDateString("fr-FR",options);
+let feteTravail = new Date("2020-05-01").toLocaleDateString("fr-FR",options);
+let victoire1945 = new Date("2020-05-08").toLocaleDateString("fr-FR",options);
+let acsension = new Date("2020-05-21").toLocaleDateString("fr-FR",options);
+let pentecote = new Date("2020-06-01").toLocaleDateString("fr-FR",options);
+let feteNationale = new Date("2020-07-14").toLocaleDateString("fr-FR",options);
+let assomption = new Date("2020-08-15").toLocaleDateString("fr-FR",options);
+let toussaint = new Date("2020-11-01").toLocaleDateString("fr-FR",options);
+let armistice = new Date("2020-11-11").toLocaleDateString("fr-FR",options);
+let noel = new Date("2020-12-25").toLocaleDateString("fr-FR",options);
+
+let array = [jourAn,pacques,feteTravail,victoire1945,acsension,pentecote,feteNationale,assomption,toussaint,armistice,noel];
+
+// console.log(array);
+
+function jourtravaille(date){    
+    // console.log(date);
+    // console.log(JourAn);
+if(dateStr == array.find(element => element == dateStr)){
+   return console.log('Non, le '+ dateStr +' est un jour ferié');
+}
+else if(getDay == 6 || getDay == 0){
+    return console.log('Non, ' + dateStr + ' est un week-end');
 }else{
-    console.log(FeteTravail);
-    console.log(date);
-    console.log(`Oui, ${getDate}  ${getMonth}  ${getYear} est un jour travaillé`);
+    return console.log('Oui, le ' + dateStr + ' est un jour travaillé');
 }
 }
 
-console.log(currentDate);
+// console.log(dateStr);
 // console.log(getDay);
-// console.log(getMonth);
-// console.log(getYear);
-// console.log(getDate);
-// console.log(JourAn);
-
-jourtravaille(currentDate);
+// jourtravaille(date);
+jourtravaille(date);
--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -2,12 +2,11 @@
 
 // let test = _POST['first'];
 let first = 3;
-let second = 3;
-
+let second = 11;
 
 function sommenombrespremiers(first, second) {
-    for(i = 2; i < first, i < second; i++){
-      if(first%i == 0 || second%i == 0){
+    for(i = 2; i <= first, i <= second; i++){
+      if(first%i == 0 ^ second%i == 0){
     return false;
       }else{
         return first + second;
